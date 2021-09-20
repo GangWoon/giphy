8f8d7482ae160e1a15376a310b08d99c9ea8ba18
GangWoon <gangwoon2018@gmail.com>
2021-09-21 04:40:09 +0900

    DetailViewStore.Reducer 구현
    Reducer 내부에서 사용하는 Environment도 같이 구현했습니다.
    - 내부 저장소로 부터 값을 읽어올 수 있는 클로저
    - id로 내부 저장소 해당 값을 변경할 수 있는 클로저

--- GIPHY/GIPHY/DetailView/DetailViewStore.swift
+++ GIPHY/GIPHY/DetailView/DetailViewStore.swift
@@ -11,17 +11,56 @@ import Combine
 final class DetailViewStore {
     
     struct State: Equatable {
-        static var empty = Self(image: nil, isFavorites: false)
+        static var empty = Self(id: "", image: nil, isFavorites: false)
+        var id: String
         var image: UIImage?
         var isFavorites: Bool
     }
     
+    struct Environment {
+        let image: UIImage?
+        let isFavorites: (String) -> Bool
+        let toggleFavorites: (Bool) -> Void
+    }
+    
+    struct Reducer {
+        
+        private let environment: Environment
+        
+        init(environment: Environment) {
+            self.environment = environment
+        }
+        
+        func reduce(
+            _ action: DetailViewController.Action,
+            state: inout State
+        ) {
+            switch action {
+            case .viewDidLoad:
+                state.image = environment.image
+                state.isFavorites = environment.isFavorites(state.id)
+                
+            case .favoritesButtonTapped:
+                state.isFavorites.toggle()
+                environment.toggleFavorites(state.isFavorites)
+            }
+        }
+    }
+    
+    private var reducer: Reducer {
+        return Reducer(environment: environment)
+    }
     let updateViewSubject: PassthroughSubject<DetailViewController.ViewState, Never>
     @Published private var state: State
+    private let environment: Environment
     private var cancellabels: Set<AnyCancellable>
     
-    init(state: State) {
+    init(
+        state: State,
+        environment: Environment
+    ) {
         self.state = state
+        self.environment = environment
         updateViewSubject = .init()
         cancellabels = []
         listenState()
