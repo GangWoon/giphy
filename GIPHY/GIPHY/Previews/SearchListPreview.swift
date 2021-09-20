e3f31bc12de28fb3d888375525e565c3cf83db7b
GangWoon <gangwoon2018@gmail.com>
2021-09-20 22:51:23 +0900

    SearchListPreview 수정
    view에서 비즈니스 로직을 제거했기 때문에 더 간단하게 UI snapshot을 테스트할 수 있습니다.

--- GIPHY/GIPHY/Previews/SearchListPreview.swift
+++ GIPHY/GIPHY/Previews/SearchListPreview.swift
@@ -10,11 +10,11 @@ import Combine
 
 struct SearchListPreview: View {
     
-    var subject: PassthroughSubject<[URL], Never> = .init()
+    var subject: PassthroughSubject<[UIImage?], Never> = .init()
     
     var body: some View {
         WrappedViewController(SearchListViewController()) { viewController in
-//            viewController.listenViewState(subject: subject)
+            viewController.listenViewState(subject: subject)
         }
         .onAppear {
             subject.send(items)
@@ -29,9 +29,16 @@ struct SearchListPreview_Previews: PreviewProvider {
 }
 
 private extension SearchListPreview {
-    var items: [URL] {
+    var items: [UIImage?] {
         return [
-            URL(string: "https://media2.giphy.com/media/3o6Zt481isNVuQI1l6/100w_s.gif?cid=68e125e87n058bexv9lh7wbikfytgpdpnzeem3lgeozebc8q&rid=100w_s.gif&ct=g")!
+            .init(systemName: "xmark"),
+            .init(systemName: "circle"),
+            .init(systemName: "pencil"),
+            .init(systemName: "scribble"),
+            .init(systemName: "pencil.tip"),
+            .init(systemName: "trash"),
+            .init(systemName: "folder.fill"),
+            .init(systemName: "paperplane.fill")
         ]
     }
 }
